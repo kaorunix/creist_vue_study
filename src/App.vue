643715f31d698ca36633f327c923d9cf1db712d9
Kaoru Sato <k.sato@creist.co.jp>
2020-12-06 01:01:21 +0900

    メニュー、メイン、フッター追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <v-app>
+    <v-navigation-drawer app>
+      メニュー
+    </v-navigation-drawer>
+    <v-app-bar color="#0182D1" app>
+      <v-toolbar-title>CREiST年末勉強会</v-toolbar-title>
+    </v-app-bar>
+    <<v-main app>
+      メイン
+    </v-main>
+    <v-footer app>
+      フッター
+    </v-footer>
   </v-app>
 </template>
 
