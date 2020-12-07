6f60a76bda6948e7a4e0bd93ec556a212c73ac4c
Kaoru Sato <k.sato@creist.co.jp>
2020-12-08 07:52:15 +0900

    ハンバーガーアイコンでの開閉実装

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
+    <v-navigation-drawer
+    v-model="adrawer"
+    app>
       メニュー
     </v-navigation-drawer>
     <v-app-bar color="#0182D1" app>
+      <v-app-bar-nav-icon @click.stop="adrawer = !adrawer"></v-app-bar-nav-icon>
       <v-toolbar-title>CREiST年末勉強会</v-toolbar-title>
     </v-app-bar>
-    <<v-main app>
+    <v-main app>
       メイン
     </v-main>
     <v-footer app>
@@ -17,6 +20,10 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      adrawer: null
+    }
+  }
 }
 </script>
