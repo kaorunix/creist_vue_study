11baae665e5ec7550a78b388d3c4d0d58391cf37
Kaoru Sato <k.sato@creist.co.jp>
2020-12-09 21:02:59 +0900

    ボタン・ドロップダウンメニューを実装

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <v-app>
     <v-navigation-drawer
-    v-model="adrawer"
+    v-model="drawer"
     app>
       メニュー
     </v-navigation-drawer>
     <v-app-bar color="#0182D1" app>
-      <v-app-bar-nav-icon @click.stop="adrawer = !adrawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>CREiST年末勉強会</v-toolbar-title>
+      <v-spacer/>
+      <v-menu offset-y>
+      <template v-slot:activator="{on}">
+      <v-btn v-on="on" color="#9182D9">会社紹介</v-btn>
+      </template>
+      <v-list>
+        <v-subheader>ヘルプ</v-subheader>
+        <v-list-item v-for="menu_item in company_menu"
+        :key="menu_item">
+          <v-list-item-content>
+            <v-list-item-title>{{ menu_item }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-menu>
     </v-app-bar>
     <v-main app>
       メイン
@@ -22,7 +37,12 @@
 export default {
   data () {
     return {
-      adrawer: null
+      drawer: null,
+      company_menu: [
+        '会社紹介',
+        '沿革',
+        '連絡先'
+      ]
     }
   }
 }
