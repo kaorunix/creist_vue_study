63d9595d10f538fd39c475ebf66a7e28a396df72
Shingo Ando <ando@creist.co.jp>
2020-12-29 15:06:46 +0900

    ちょっと編集

--- src/views/About2.vue
+++ src/views/About2.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="sa">
-    <h1>This is an about page2</h1>
-  </div>
+  <v-app>
+    <v-btn>ボタンです。長い。</v-btn>
+    <div class="about2">
+      <h1>{{ message }}</h1>
+    </div>
+  </v-app>
 </template>
+
+<script>
+export default {
+  name: 'About2',
+  data () {
+    return {
+      message: 'This is an about vue page2'
+    }
+  }
+}
+</script>
